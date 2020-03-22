c9f5460a1992b9176d3ff1c3e80084de32657bdf
kang4609 <kang4609@gmail.com>
2020-03-22 20:12:15 +0900

    commit

--- blog-frontend/src/modules/index.js
+++ blog-frontend/src/modules/index.js
@@ -1,13 +1,15 @@
 import { combineReducers } from 'redux';
 import { all } from 'redux-saga/effects';
 import auth, { authSaga } from './auth';
-import loading from './loading';
 import user, { userSaga } from './user';
+import write from './write';
+import loading from './loading';
 
 const rootReducer = combineReducers({
   auth,
   user,
   loading,
+  write
 });
 
 export function* rootSaga() {
--- blog-frontend/src/pages/WritePage.js
+++ blog-frontend/src/pages/WritePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Editor from '../components/write/Editor';
+import EditorContainer from '../containers/write/EditorContainer';
 import TagBox from '../components/write/TagBox';
 import WriteActionButtons from '../components/write/WriteActionButtons';
 import Responsive from '../components/common/Responsive';
@@ -7,7 +7,7 @@ import Responsive from '../components/common/Responsive';
 const WritePage = () => {
   return (
     <Responsive>
-      <Editor />
+      <EditorContainer />
       <TagBox />
       <WriteActionButtons />
     </Responsive>
